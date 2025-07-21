17658ae841b8516c5fdb712b663be2640fa9b435
Naresh <Naresh@DESKTOP-RTTUOKQ>
2025-07-21 22:45:41 +0530

    Basics And Looping

---
+++ C2TC_COREJAVA/src/day1/Looping/IfDemo.java
@@ -0,0 +1,13 @@
+package day1.Looping;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x = 5;
+		if (x != 5)
+			System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+
+	}
+
+}
---
+++ C2TC_COREJAVA/src/day1/Looping/WhileDemo.java
@@ -0,0 +1,15 @@
+package day1.Looping;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i=75;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
