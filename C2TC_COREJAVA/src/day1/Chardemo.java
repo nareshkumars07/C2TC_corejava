8216294deeb76eb52c196bb5f9f295d8929b61c8
Naresh <Naresh@DESKTOP-RTTUOKQ>
2025-07-21 22:11:05 +0530

    Day 1 demos

---
+++ C2TC_COREJAVA/src/day1/Chardemo.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class Chardemo {
+
+	public static void main(String[] args) {
+			// assigning single character literal
+			char ch = 'n';
+			System.out.println(ch);
+
+			// assigning number to char
+			char ch1 = 77;
+			System.out.println(ch1);
+
+			// assigning unicode to char
+			char var1 = '\u00A7';
+			System.out.println(var1);
+
+			// Unicode representation
+			char var2 = '\u20AC';
+			System.out.println(var2);
+
+			// ASCI code representation
+			int a = 'N';
+			System.out.println(a);
+		}
+	}
